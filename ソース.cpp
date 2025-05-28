11ee8e0d3ccd4df904e31fcd0c57356eee38206b
hiroshi.nakai <hiroshi.nakai@hirorosoft.com>
2025-05-28 15:33:12 +0900

    DrawPixelで点で資格を描画

--- ソース.cpp
+++ ソース.cpp
@@ -1,10 +1,10 @@
 
 #include <Dxlib.h>
 
-//int main(void) 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-	PSTR lpCmdLine, int nCmdShow)
-{
+#define RED_COLOR	GetColor(255, 0, 0)
+
+int init() {
+
 #ifndef _DEBUG
 	// ログ抑制
 	SetOutApplicationLogValidFlag(FALSE);
@@ -13,11 +13,32 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	int result = ChangeWindowMode(TRUE);		// ウインドモードで起動 640*480
 	SetMainWindowText("DxLibテストだべ。");
 
+
 	// グラフィックウィンドウへ変更
 	if (DxLib_Init() == -1) return -1;
 
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	PSTR lpCmdLine, int nCmdShow)
+{
+	if (init() == -1) return -1;
+
 	printfDx("Hello World\n");
-//	(void)getchar();
+
+	int r = 255, g, b = 0;
+
+	for (int y = 0; y < 256; y++, r--)
+	{
+		g = 255;
+		for (int x = 0; x < 256; x++, g--, b++)
+		{
+			DrawPixel(192 + x, 112 + y, GetColor(r, g, b));
+		}
+	}
+	
+	
 	WaitKey();
 
 
@@ -25,3 +46,4 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
 	return 0;
 }
+
