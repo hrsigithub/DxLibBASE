43b33d8de0223eab8c63a647446e615ba4b55156
hiroshi.nakai <hiroshi.nakai@hirorosoft.com>
2025-05-28 14:04:35 +0900

    グラフィクウィンドウに表示する。文字コード変更。マルチバイトへ。

--- ソース.cpp
+++ ソース.cpp
@@ -3,8 +3,18 @@
 
 int main(void) {
 
-	printf("Hello World\n");
-	(void)getchar();
+	int result = ChangeWindowMode(TRUE);		// ウインドモードで起動 640*480
+	SetMainWindowText("DxLibテストだべ。");
+
+	// グラフィックウィンドウへ変更
+	if (DxLib_Init() == -1) return -1;
+
+	printfDx("Hello World\n");
+//	(void)getchar();
+	WaitKey();
+
+
+	DxLib_End();
 
 	return 0;
 }
